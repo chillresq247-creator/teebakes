a407089e7771044619b8f1160c590b1bd2e2edab
chillresq247-creator <chillresq247@gmail.com>
2026-06-19 19:12:29 +0100

    Update sumup.js

--- api/sumup.js
+++ api/sumup.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
       },
       body: JSON.stringify({
         order_id: orderId,
-        amount: Number(amount).toFixed(2),
+        amount: Number(amount),
         currency,
         pay_to_email: "donutvanman@gmail.com",
         description: `TeeBakes Order ${orderId}`,
