b0dcb481406bcd096837d65de36107591d2d14b1
chillresq247-creator <chillresq247@gmail.com>
2026-08-27 11:58:48 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client';
 
 import App from './App';
 
+if (typeof window !== 'undefined' && window.location.search.includes('admin')) {
+  const manifestLink = document.querySelector('link[rel="manifest"]');
+  if (manifestLink) manifestLink.setAttribute('href', '/manifest-admin.json');
+}
+
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
